2914e13f9cc9bf5eefd5704ef59ee244df36fc5b
Arseniy <arsikkor12@yandex.ru>
2024-06-08 00:18:08 +0300

    Конец 1 инкремента

--- internal/incriment1/metricStorage/metricStorage.go
+++ internal/incriment1/metricStorage/metricStorage.go
@@ -55,17 +55,15 @@ func validationFirstElementURL(update string, name string, w http.ResponseWriter
 func validationTypeMetric(typeMetric string, nameMetric string, countMetric string, w http.ResponseWriter) bool {
 	switch typeMetric { //Свитч для проверки что это запрос или gauge или counter
 	case "gauge": //Если передано значение 'gauge'
-		{
-			count, err := strconv.ParseFloat(countMetric, 64) //Проверка что переданно число и его можно перевети в float64
-			if err != nil {                                   //Если не удалось перевести
-				http.Error(w, "Это не Float", http.StatusBadRequest) //Вывод error-ки
-				return true                                          //
-			}
-			Metric.UpdateGauge(nameMetric, count)
-			fmt.Println("Это gauge запрос")
-			fmt.Println(Metric.gauge)
-			return false
+		count, err := strconv.ParseFloat(countMetric, 64) //Проверка что переданно число и его можно перевети в float64
+		if err != nil {                                   //Если не удалось перевести
+			http.Error(w, "Это не Float", http.StatusBadRequest) //Вывод error-ки
+			return true                                          //
 		}
+		Metric.UpdateGauge(nameMetric, count)
+		fmt.Println("Это gauge запрос")
+		fmt.Println(Metric.gauge)
+		return false
 	case "counter": //Если передано значение 'counter'
 		{
 			count, err := strconv.ParseInt(countMetric, 10, 64) //Проверка что переданно число и его можно перевети в int64
@@ -93,10 +91,8 @@ func (m *MetricStorage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if validationLengthsURL(parts, w) {
 		return
 	}
-	update := parts[1]      //Создание переменных для удобного использования в коде
-	typeMetric := parts[2]  //Создание переменных для удобного использования в коде
-	nameMetric := parts[3]  //Создание переменных для удобного использования в коде
-	countMetric := parts[4] //Создание переменных для удобного использования в коде
+
+	update, typeMetric, nameMetric, countMetric := parts[1], parts[2], parts[3], parts[4]
 	if validationFirstElementURL(update, "update", w) {
 		return
 	}
