a0138e1770847208bb1aa5f944911988cf2c1af0
Arseniy <arsikkor12@yandex.ru>
2024-07-09 14:03:35 +0300

    Без изменений

--- internal/handlers/middleware/middleware.go
+++ internal/handlers/middleware/middleware.go
@@ -18,10 +18,10 @@ func GzipMiddleware(next http.Handler) http.Handler {
 				return
 			}
 			defer gz.Close()
-			//w.Header().Set("Content-Encoding", "gzip") зачем я это ПИСАЛ??!?!?!?!?!?!
 			// Замена r.Body на распакованный stream
 			r.Body = io.NopCloser(gz)
 		}
+		//w.Header().Set("Content-Encoding", "gzip") //зачем я это ПИСАЛ??!?!?!?!?!?!
 		next.ServeHTTP(w, r)
 	})
 }
