2cf0e7d07079877e062d2ba47d5642236cb51638
Home PS <arsikkor12@yandex.ru>
2024-09-28 17:03:14 +0300

    Исправление импортов

--- internal/funcAgent/sendMetrics.go
+++ internal/funcAgent/sendMetrics.go
@@ -12,10 +12,12 @@ import (
 	"time"
 )
 
+var cfgFlags = config.VarFlag{}
+
 // SendingMetric Функция которая каджые $FlagPollInterval секунд запускает функию по отправке метрик
 func SendingMetric(gaugeMetrics map[string]float64, counterMetrics map[string]int64) {
-	ticker1 := time.NewTicker(time.Duration(*config.FlagPollInterval) * time.Second)
-	ticker2 := time.NewTicker(time.Duration(*config.FlagRreportInterval) * time.Second)
+	ticker1 := time.NewTicker(time.Duration(cfgFlags.FlagPollInterval) * time.Second)
+	ticker2 := time.NewTicker(time.Duration(cfgFlags.FlagReportInterval) * time.Second)
 	defer ticker1.Stop()
 	defer ticker2.Stop()
 
@@ -23,11 +25,11 @@ func SendingMetric(gaugeMetrics map[string]float64, counterMetrics map[string]in
 		select {
 		case <-ticker1.C:
 			gaugeMetrics, counterMetrics = UpdateMetric()
-			fmt.Printf("Пауза в %d секунд между сборкой метрик\n", *config.FlagPollInterval)
+			fmt.Printf("Пауза в %d секунд между сборкой метрик\n", cfgFlags.FlagPollInterval)
 		case <-ticker2.C:
 			SendJSONMetricsGauge(gaugeMetrics)
 			SendJSONMetricsCounter(counterMetrics)
-			fmt.Printf("Пауза в %d секунд между отправкой метрик на сервер\n", *config.FlagRreportInterval)
+			fmt.Printf("Пауза в %d секунд между отправкой метрик на сервер\n", cfgFlags.FlagReportInterval)
 		}
 	}
 }
@@ -35,7 +37,7 @@ func SendingMetric(gaugeMetrics map[string]float64, counterMetrics map[string]in
 // SendJSONMetricsGauge Отправляет метрики типа Gauge по по url
 func SendJSONMetricsGauge(mapMetric map[string]float64) {
 	client := resty.New()
-	url := fmt.Sprintf("http://%s/update/", *config.FlagAddressAndPort)
+	url := fmt.Sprintf("http://%s/update/", cfgFlags.FlagAddressAndPort)
 
 	for nameMetric, value := range mapMetric {
 		metrics := metricStorage.Metrics{
@@ -72,7 +74,7 @@ func SendJSONMetricsGauge(mapMetric map[string]float64) {
 // SendJSONMetricsCounter Отправляет метрики типа Gauge по по url
 func SendJSONMetricsCounter(mapMetric map[string]int64) {
 	client := resty.New()
-	url := fmt.Sprintf("http://%s/update/", *config.FlagAddressAndPort)
+	url := fmt.Sprintf("http://%s/update/", cfgFlags.FlagAddressAndPort)
 
 	for nameMetric, value := range mapMetric {
 		metrics := metricStorage.Metrics{
