0ae5a1296b8fee83646974f9445a2f8b0ea334e4
Home PS <arsikkor12@yandex.ru>
2024-10-27 15:17:27 +0300

    Логи

--- internal/storage/loadMetric/loadmetric.go
+++ internal/storage/loadMetric/loadmetric.go
@@ -2,6 +2,7 @@ package loadMetric
 
 import (
 	"github.com/LI-SeNyA-vE/KursMetrics/internal/config"
+	"github.com/LI-SeNyA-vE/KursMetrics/internal/middleware/logger"
 	"github.com/LI-SeNyA-vE/KursMetrics/internal/storage/dataBase"
 	metricStorage "github.com/LI-SeNyA-vE/KursMetrics/internal/storage/metricStorage"
 )
@@ -12,8 +13,12 @@ func InitializeStorage() {
 	if config.ConfigServerFlags.FlagDatabaseDsn != "" {
 		err = dataBase.LoadMetricFromDB()
 	}
+	if err != nil {
+		logger.Log.Infof("Ошибка получена из функции LoadMetricFromDB: %s | флаг FlagRestore: %v", err, config.ConfigServerFlags.FlagRestore)
+	}
 
 	if err != nil && config.ConfigServerFlags.FlagRestore {
+		logger.Log.Info("Зашли в функцию LoadMetricFromFile")
 		metricStorage.LoadMetricFromFile(config.ConfigServerFlags.FlagFileStoragePath)
 	}
 	return
